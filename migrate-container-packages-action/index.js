4d91fae17b5489bc0a81219e443641cda943b739
Jesse Antoszyk <22500761+jcantosz@users.noreply.github.com>
2025-05-23 13:29:24 -0400

    rebuiold old version

--- migrate-container-packages-action/index.js
+++ migrate-container-packages-action/index.js
@@ -1,8 +1,9 @@
 /**
  * This file is the entrypoint for the action
  */
-// import { run } from "./src/main.js";
-
 console.log("Running the thing");
+import { run } from "./src/main.js";
+
+console.log("Running the thing 2x");
 // It calls the actual logic of the action
-// run();
+run();
