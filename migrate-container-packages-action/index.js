06ecdf28120e0a9a7147a07efbdbe21306519086
Jesse Antoszyk <22500761+jcantosz@users.noreply.github.com>
2025-05-23 13:31:16 -0400

    build with package.json

--- migrate-container-packages-action/index.js
+++ migrate-container-packages-action/index.js
@@ -1,9 +1,7 @@
 /**
  * This file is the entrypoint for the action
  */
-console.log("Running the thing");
 import { run } from "./src/main.js";
 
-console.log("Running the thing 2x");
 // It calls the actual logic of the action
 run();
