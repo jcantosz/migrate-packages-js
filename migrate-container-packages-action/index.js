fda1b5da2a9bea7bccfa5bc0e367218cd54307e1
Jesse Antoszyk <22500761+jcantosz@users.noreply.github.com>
2025-05-23 13:27:14 -0400

    run the thing

--- migrate-container-packages-action/index.js
+++ migrate-container-packages-action/index.js
@@ -1,8 +1,8 @@
 /**
  * This file is the entrypoint for the action
  */
-import { run } from "./src/main.js";
+// import { run } from "./src/main.js";
 
 console.log("Running the thing");
 // It calls the actual logic of the action
-run();
+// run();
